a3bf31272481ec41e475df1b24acb999e36b4b9e
slava <trustworthy@tuta.io>
2024-12-10 09:41:08 +0200

    first commit

---
+++ Module1.vb
@@ -0,0 +1,24 @@
+﻿Imports OpenQA.Selenium.Firefox
+Imports OpenQA.Selenium
+Module Module1
+
+    Sub Main()
+        Dim FirefoxOptions As New FirefoxOptions()
+        FirefoxOptions.SetPreference("binary", "C:\Program Files\Mozilla Firefox\firefox.exe")
+        Dim Driver As IWebDriver = New FirefoxDriver(FirefoxOptions)
+        Driver.Navigate().GoToUrl("https://www.vb-net.com/")
+        Dim HtmlElement As IWebElement
+        Dim Id = "order"
+        Try
+            HtmlElement = Driver.FindElement(By.Id(Id))
+            Console.WriteLine(HtmlElement.Text)
+            HtmlElement.Click()
+            'Dim attributeValue As String = htmlElement.GetDomAttribute("yourAttributeName")
+            'Dim propertyValue As String = htmlElement.GetDomProperty("yourPropertyName")
+        Catch ex As NoSuchElementException
+            Console.WriteLine($"Element with ID '{Id}' not found.")
+        End Try
+        Console.ReadLine()
+    End Sub
+
+End Module
---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("SeleniumTest")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("SeleniumTest")>
+<Assembly: AssemblyCopyright("Copyright ©  2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("b710bb9e-490c-4de6-af18-22a75ab24c64")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
